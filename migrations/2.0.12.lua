b378e8e0e7662bdf24ca5aca132391b060b93332
RedRafe <gabri97.gt+redrafe@gmail.com>
2024-10-25 17:06:46 +0200

    release v2.0.13

--- migrations/2.0.12.lua
+++ migrations/2.0.12.lua
@@ -1,13 +1,9 @@
 local size = settings.startup['ts_solar_system_size'].value
 local size_to_index = { small = 3, medium = 6, large = 9 }
 
-log('Before: Is space location unlocked? ' .. (game.forces.player_13.is_space_location_unlocked('naedris') and 'true' or 'false'))
-
 for i = 1, (3 * size_to_index[size]) do
   local f = game.forces['player_'..i]
   if f and f.valid then
     f.reset_technology_effects()
   end
 end
-
-log('After: Is space location unlocked? ' .. (game.forces.player_13.is_space_location_unlocked('naedris') and 'true' or 'false'))
